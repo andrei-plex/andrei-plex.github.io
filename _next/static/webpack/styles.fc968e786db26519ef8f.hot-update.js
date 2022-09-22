408ca2b23ac735e2858ec8a18fe134d9940f13b4
Andrei Kucherov <a.kucherov@pixelplex.io>
2022-09-22 13:58:44 +0300

    fix2

---
+++ _next/static/webpack/styles.fc968e786db26519ef8f.hot-update.js
@@ -0,0 +1,273 @@
+webpackHotUpdate("styles",{
+
+/***/ "./src/components/accordion-block-with-columns/styles.module.scss":
+false,
+
+/***/ "./src/components/breadcrumbs-services/styles.module.scss":
+false,
+
+/***/ "./src/components/faq/styles.module.scss":
+false,
+
+/***/ "./src/components/landings/scroll-to-form/styles.module.scss":
+false,
+
+/***/ "./src/components/optimize-for-seo/relevant-projects/styles.scss":
+false,
+
+/***/ "./src/components/our-clients/styles.module.scss":
+false,
+
+/***/ "./src/components/partner-slider/styles.module.scss":
+false,
+
+/***/ "./src/components/ready-to-talk/styles.module.scss":
+false,
+
+/***/ "./src/components/redesign-components/blocks-with-small-image/block/styles.module.scss":
+false,
+
+/***/ "./src/components/redesign-components/blocks-with-small-image/styles.module.scss":
+false,
+
+/***/ "./src/components/redesign-components/development-expertise-slider/slider/styles.module.scss":
+false,
+
+/***/ "./src/components/redesign-components/development-expertise-slider/styles.module.scss":
+false,
+
+/***/ "./src/components/redesign-components/keen-slider/slider/slide/styles.module.scss":
+false,
+
+/***/ "./src/components/redesign-components/keen-slider/slider/styles.module.scss":
+false,
+
+/***/ "./src/components/redesign-components/keen-slider/styles.module.scss":
+false,
+
+/***/ "./src/components/redesign-components/relevant-projects/styles.module.scss":
+false,
+
+/***/ "./src/components/redesign-components/slider-with-image-block/slider/styles.module.scss":
+false,
+
+/***/ "./src/components/redesign-components/slider-with-image-block/styles.module.scss":
+false,
+
+/***/ "./src/components/redesign-components/steps-block/steps/styles.module.scss":
+false,
+
+/***/ "./src/components/redesign-components/steps-block/styles.module.scss":
+false,
+
+/***/ "./src/components/redesign-components/tab-slider/styles.module.scss":
+false,
+
+/***/ "./src/containers/home/achievements/styles.module.scss":
+false,
+
+/***/ "./src/containers/home/deliver-value/styles.module.scss":
+false,
+
+/***/ "./src/containers/home/echo-block/styles.module.scss":
+false,
+
+/***/ "./src/containers/home/home-header/styles.module.scss":
+false,
+
+/***/ "./src/containers/home/map/card/styles.module.scss":
+false,
+
+/***/ "./src/containers/home/map/styles.module.scss":
+false,
+
+/***/ "./src/containers/home/success-stories/case-slider/styles.module.scss":
+false,
+
+/***/ "./src/containers/home/success-stories/styles.module.scss":
+false,
+
+/***/ "./src/containers/home/tech-service-majors-highlighted/styles.module.scss":
+false,
+
+/***/ "./src/containers/mobile-development/description-platforms/style.module.scss":
+false,
+
+/***/ "./src/containers/mobile-development/platforms-title/accordion-button/styles.module.scss":
+false,
+
+/***/ "./src/containers/mobile-development/platforms-title/styles.module.scss":
+false,
+
+/***/ "./src/containers/mobile-development/platforms/accordion/styles.module.scss":
+false,
+
+/***/ "./src/containers/mobile-development/platforms/styles.module.scss":
+false,
+
+/***/ "./src/containers/mobile-development/projects/styles.module.scss":
+false,
+
+/***/ "./src/containers/mobile-development/solutions/numbers/styles.module.scss":
+false,
+
+/***/ "./src/containers/mobile-development/solutions/solution-block/styles.module.scss":
+false,
+
+/***/ "./src/containers/mobile-development/solutions/styles.module.scss":
+false,
+
+/***/ "./src/containers/mobile-development/styles.module.scss":
+false,
+
+/***/ "./src/containers/mobile-development/technologies-tools/accordion-content/icons-container/styles.module.scss":
+false,
+
+/***/ "./src/containers/mobile-development/technologies-tools/accordion-content/styles.module.scss":
+false,
+
+/***/ "./src/containers/mobile-development/technologies-tools/styles.module.scss":
+false,
+
+/***/ "./src/containers/nft-development/numbers/styles.module.scss":
+false,
+
+/***/ "./src/containers/services/ai/styles.module.scss":
+false,
+
+/***/ 100:
+false,
+
+/***/ 101:
+false,
+
+/***/ 102:
+false,
+
+/***/ 103:
+false,
+
+/***/ 22:
+false,
+
+/***/ 23:
+false,
+
+/***/ 30:
+false,
+
+/***/ 31:
+false,
+
+/***/ 32:
+false,
+
+/***/ 33:
+false,
+
+/***/ 34:
+false,
+
+/***/ 35:
+false,
+
+/***/ 36:
+false,
+
+/***/ 37:
+false,
+
+/***/ 38:
+false,
+
+/***/ 39:
+false,
+
+/***/ 40:
+false,
+
+/***/ 63:
+false,
+
+/***/ 65:
+false,
+
+/***/ 66:
+false,
+
+/***/ 67:
+false,
+
+/***/ 70:
+false,
+
+/***/ 71:
+false,
+
+/***/ 73:
+false,
+
+/***/ 74:
+false,
+
+/***/ 75:
+false,
+
+/***/ 76:
+false,
+
+/***/ 77:
+false,
+
+/***/ 78:
+false,
+
+/***/ 80:
+false,
+
+/***/ 81:
+false,
+
+/***/ 82:
+false,
+
+/***/ 83:
+false,
+
+/***/ 84:
+false,
+
+/***/ 85:
+false,
+
+/***/ 87:
+false,
+
+/***/ 88:
+false,
+
+/***/ 89:
+false,
+
+/***/ 90:
+false,
+
+/***/ 91:
+false,
+
+/***/ 92:
+false,
+
+/***/ 96:
+false,
+
+/***/ 97:
+false,
+
+/***/ 98:
+false,
+
+/***/ 99:
+false
+
+})
